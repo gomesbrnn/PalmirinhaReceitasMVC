ac7c8b6bc1fcbc7bc7501bcfecaac3a619a59fb8
Breno Gomes <78120805+gomesbrnn@users.noreply.github.com>
2022-07-14 22:58:32 -0300

    Fix policies

---
+++ GerenciamentoController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DESAFIO_MVC.Models;
+using DESAFIO_MVC.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DESAFIO_MVC.Controllers
+{
+    [Authorize(Policy = "AdminRole")]
+    public class GerenciamentoController : Controller
+    {
+        private readonly ApplicationDbContext database;
+
+        public GerenciamentoController(ApplicationDbContext database)
+        {
+            this.database = database;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
